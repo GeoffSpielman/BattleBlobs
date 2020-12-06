c05e59e51825616a8d47d38c95f8bd0735ade89c
GeoffSpielman <geoff.spielman21@gmail.com>
2020-12-05 22:51:14 -0500

    Whiteboarded 'place ships' and 'validate ship' algorithms, started vue project

---
+++ battleship/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="outermost">
+    <div id="horizontalMiddle">
+      <div class="shipBuilderContainer">
+      <ship-builder> </ship-builder>
+      </div>
+      <div class="shipBuilderContainer">
+      <ship-builder> </ship-builder>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import shipBuilder from "./components/shipBuilder.vue";
+
+export default {
+  name: "App",
+  components: {
+    "ship-builder": shipBuilder,
+  },
+};
+</script>
+
+
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0px;
+}
+#outermost {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+#horizontalMiddle{
+  height: 38%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.shipBuilderContainer{
+  width: 25%;
+  height: 100%;
+   margin: 0px 30px 0px 30px;
+}
+</style>
---
+++ battleship/src/components/shipBuilder.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="outermost">
+    <div id="buttonGrid">
+      <div v-for="(rowArray, rowIdx) in canvasGrid" :key="rowIdx" class="rowDiv">
+        <button v-for="(btn, colIdx) in rowArray" :key="colIdx" class="gridBtn">
+          {{ rowIdx + ", " + colIdx }}
+        </button>
+      </div>
+    </div>
+    <p id="outputStatement">Looks great!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      canvasGrid: [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+#outermost {
+  background-color: lightblue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.rowDiv{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.gridBtn{
+    height: 40px;
+    width: 40px;
+}
+
+#outputStatement{
+    text-align: center;
+}
+</style>
