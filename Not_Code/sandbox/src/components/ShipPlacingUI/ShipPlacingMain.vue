09f7ac6989692a5b33d67688bfde3718fb6ea33a
GeoffSpielman <geoff.spielman21@gmail.com>
2020-12-29 19:55:44 -0500

    drop points are correctly calculated and the drop zone is higlighted, ready to start implementing the algorithm

--- Not_Code/sandbox/src/components/ShipPlacingUI/ShipPlacingMain.vue
+++ Not_Code/sandbox/src/components/ShipPlacingUI/ShipPlacingMain.vue
@@ -21,8 +21,11 @@
     <div id="mainRow">
       <div id="leftStatusCol">
         <h3>Status:</h3>
-        <p>numRows:</p>
-        <p>numCols:</p>
+        <p>numRows: {{numRows}}</p>
+        <p>numCols: {{numCols}}</p>
+        <br>
+        <p>curShip: {{curShip}}</p>
+        <br>
         <label for="nextDrop"> Next drop point: </label>
         <input
           type="text"
@@ -30,6 +33,7 @@
           id="nextDrop"
           name="nextDrop"
         />
+        
       </div>
       <div id="centerGridCol">
         <div v-for="(rowArray, rowIdx) in map" :key="rowIdx" class="mapRowDiv">
@@ -39,7 +43,7 @@
             @click="mapButtonClicked(rowIdx, colIdx)"
             :class="[
               'mapBtn',
-              { mapSquareOccupied: map[rowIdx][colIdx] === 1 },
+              { mapSquareOccupied: map[rowIdx][colIdx] > 0 },
             ]"
             :id="'btn-' + rowIdx + '-' + colIdx"
           >
@@ -184,20 +188,28 @@ export default {
       numCols: 14,
       map: [],
       dropPoint: [0,0],
+      curShip: 0,
     };
   },
   methods: {
     updateShipOffets(ID, newOffsets) {
       this.ships[ID].offsets = newOffsets;
     },
+
     updateShipRanges(ID, newRowMin, newRowMax, newColMin, newColMax) {
       this.ships[ID].rowMin = newRowMin;
       this.ships[ID].rowMax = newRowMax;
       this.ships[ID].colMin = newColMin;
       this.ships[ID].colMax = newColMax;
     },
+
+    randDropPointAsString(){
+      return (Math.floor(Math.random() * (this.ships[this.curShip].rowMax - this.ships[this.curShip].rowMin + 1))  + this.ships[this.curShip].rowMin).toString() +
+      "," +
+      (Math.floor(Math.random() * (this.ships[this.curShip].colMax - this.ships[this.curShip].colMin + 1))  + this.ships[this.curShip].colMin).toString();
+    },
+
     mapButtonClicked(rowIdx, colIdx) {
-      console.log("clicked [" + rowIdx + "," + colIdx + "]");
       if (this.map[rowIdx][colIdx] === 0) {
         this.map[rowIdx][colIdx] = 1;
       } else {
@@ -223,10 +235,7 @@ export default {
 
       */
       //put the next drop point into the input box (which I can change if needed)
-      this.$refs.nextDropPointTxt.value =
-      (Math.floor(Math.random() * (this.numRows- 1))  - 1).toString() +
-      "," +
-      (Math.floor(Math.random() * (this.numCols - 1)) - 1).toString();
+      this.$refs.nextDropPointTxt.value = this.randDropPointAsString()
     },
 
     formatDropZoneBorders(dropPoint, borderWidth, borderColour){
@@ -236,21 +245,43 @@ export default {
         for (i = 0; i < 4; i++) {
         if (dropPoint[1] + i < 0 || dropPoint[1] + i > (this.numCols - 1)) continue;
 
-        console.log("Looking for a button with an id of: btn-" + dropPoint[0] + "-" + (dropPoint[1] + i));
         let btn = document.getElementById("btn-" + dropPoint[0] + "-" + (dropPoint[1] + i))
         btn.style.borderTop = borderWidth + " solid " + borderColour;
         }
       }
       //bottom borders
+      if (dropPoint[0] + 3 < this.numRows){
+        for (i = 0; i < 4; i ++){
+          if (dropPoint[1] + i < 0 || dropPoint[1] + i > (this.numCols - 1)) continue;
 
+          let btn = document.getElementById("btn-" + (dropPoint[0] + 3) + "-" + (dropPoint[1] + i))
+          btn.style.borderBottom = borderWidth + " solid " + borderColour;
+        }
+      }
+      //left borders
+      if (dropPoint[1] >= 0){
+        for (i = 0; i < 4; i++){
+          if (dropPoint[0] + i < 0 || dropPoint[0] + i > (this.numRows - 1)) continue;
+
+          let btn = document.getElementById("btn-" + (dropPoint[0] + i) + "-" + dropPoint[1])
+          btn.style.borderLeft = borderWidth + " solid " + borderColour;
+        }
+      }
+      //right borders
+      if (dropPoint[1] + 3 < this.numCols){
+        for (i = 0; i < 4; i++){
+          if (dropPoint[0] + i < 0 || dropPoint[0] + i > (this.numRows - 1)) continue;
+
+          let btn = document.getElementById("btn-" + (dropPoint[0] + i) + "-" + (dropPoint[1] + 3))
+          btn.style.borderRight = borderWidth + " solid " + borderColour;
+        }
+      }
     }
       
   },
 
   watch: {
     dropPoint(dropPoint, prevDropPoint) {
-      console.log("dropPoint watcher fired. prev: " + prevDropPoint + "    new: " + dropPoint)
-
       if (
         dropPoint[0] < -1 ||
         dropPoint[0] > this.numRows - 3 ||
@@ -262,10 +293,8 @@ export default {
         );
         return;
       }
-      
       this.formatDropZoneBorders(prevDropPoint, '1px', 'black');
-      this.formatDropZoneBorders(dropPoint, '4px', 'yellow');
-      
+      this.formatDropZoneBorders(dropPoint, '3px', this.ships[this.curShip].colour);
     }
   },
 
@@ -275,11 +304,10 @@ export default {
       .map(() => new Array(this.numCols).fill(0));
   },
   mounted() {
-    this.$refs.nextDropPointTxt.value =
-      (Math.floor(Math.random() * (this.numRows- 1))  - 1).toString() +
-      "," +
-      (Math.floor(Math.random() * (this.numCols - 1)) - 1).toString();
+    this.$refs.nextDropPointTxt.value = this.randDropPointAsString()
   },
+
+  
 };
 </script>
 
@@ -329,7 +357,7 @@ h3 {
   flex-grow: 1;
 }
 
-.rowDiv {
+.mapRowDiv {
   display: flex;
   flex-direction: row;
   justify-content: center;
