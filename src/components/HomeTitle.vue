8b8ab3d65c344a2780a078200d95e387c591f1f7
Geoff Spielman <geoff.spielman21@gmail.com>
2021-02-21 22:24:50 -0500

    Home page works in production now! Switched opacity animations to transitions

--- src/components/HomeTitle.vue
+++ src/components/HomeTitle.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="titleOuterDiv">
-    <transition name="welcomeFade" appear :duration="2200"> 
+    <transition name="welcomeFade" appear> 
     <h2 id="welcomeDiv">Welcome to</h2>
     </transition>
-    <transition name="titleFade" appear>
+    <transition name="titleGrow" appear>
       <h1 id="titleDiv">CLUSTERFUCK</h1>
     </transition>
   </div>
@@ -14,7 +14,6 @@
   display: flex;
   align-items: center;
   flex-direction: column;
-
   height: 180px;
   background-color: #0d47a1;
   color: white;
@@ -28,38 +27,28 @@
 
 #titleDiv {
   font-size: 65pt;
-  width: 750px;
   text-align: center;
   overflow: hidden;
   font-family: Copperplate Gothic, Tahoma, sans-serif;
   margin-top: -12px;
 }
 
-.welcomeFade.enter{
+.welcomeFade-enter{
   opacity: 0;
 }
+
 .welcomeFade-enter-active{
-  animation: fade-in-welcome 2s linear forwards;
-  animation-delay: 0.2s;
+  transition: opacity 2.5s;
+  transition-delay: 0.1s;
+}
+
+.titleGrow-enter-active {
+  animation: grow-in-title 2.5s ease-out;
+  animation-delay: 1s;
   animation-fill-mode: backwards;
 }
 
-#titleDiv.titleFade-enter-active {
-  animation: fade-in-title 2.5s ease-out;
-  animation-delay: 1.2s;
-  animation-fill-mode: backwards;
-}
-
-@keyframes fade-in-welcome{
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 100%;
-  }
-}
-
-@keyframes fade-in-title {
+@keyframes grow-in-title {
   from {
     width: 0px;
   }
