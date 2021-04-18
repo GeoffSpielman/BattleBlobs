ac555c9dad27ac05e110e12d2c33f11313395a8b
Geoff Spielman <geoff.spielman21@gmail.com>
2021-04-17 23:51:17 -0400

    chat message entry UI done, stuck on Enter key propagation issue

--- src/components/game/IndividualChatPane.vue
+++ src/components/game/IndividualChatPane.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="individualChatPaneOutermost">
-    <h5 id="chatHeader"> PRIVATE CHAT WITH {{otherPlayer}}</h5>
-    
+    <h5 id="chatHeader">PRIVATE CHAT WITH {{ otherPlayer }}</h5>
+    <div id="convoArea">Messages go here</div>
+    <message-entry></message-entry>
   </div>
 </template>
 
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import MessageEntry from "@/components/game/MessageEntry.vue";
 
 @Component({
   name: "IndividualChatPane",
+  components: {
+    MessageEntry,
+  },
 })
 export default class IndividualChatPane extends Vue {
   otherPlayer: string = "Bob".toUpperCase();
-
 }
 </script>
 
@@ -28,11 +32,16 @@ export default class IndividualChatPane extends Vue {
   background-color: #f5f8fa;
 }
 
-#chatHeader{
+#chatHeader {
   width: 100%;
   text-align: center;
-  background-color: #BBDEFB;
+  background-color: #bbdefb;
 }
 
+#convoArea {
+  flex-grow: 1;
+  display: flex;
+  width: 100%;
+}
 
 </style>
